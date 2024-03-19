0b6600019126c75b5a60617a0b53721ca69acfa8
kim-dajeong <kimdajeong10@gmail.com>
2024-03-19 12:45:02 -0700

    cpp

---
+++ src/linkstate.cpp
@@ -0,0 +1,55 @@
+/**
+ * @file linkstate.c
+ *  @brief 
+ *
+ *  @author Ana Bandari (anabandari)
+ *  @author Dajeong Kim (dkim2)
+ * 
+ *  @bug 
+ * 
+ */
+
+
+
+/*   Includes   */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <errno.h>
+
+
+
+/**
+ * @brief 
+ * @param 
+ * @param 
+ * 
+ * @return 
+ * 
+ * 
+*/
+void lsr(){
+    
+    
+
+}
+
+/**
+ * @brief main function 
+ * 
+ * @param arcg argument count, indicating number of arguments passed from the command line
+ * @param argv argument vector, pointing to an array of strings, each of which contains an argument passed from the command line
+ * 
+ * @return 0
+ * 
+*/
+int main(int argc, char** argv){
+
+  
+}
